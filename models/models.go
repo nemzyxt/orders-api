0dba22d8b5747af8a7ead1e7dafdba1aab66604e
nemzyxt <bR0S3@protonmail.com>
2022-12-25 09:48:08 +0300

    Create Order model

---
+++ models/models.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type Order struct {
+	OrderID      uint        `json:"orderId" gorm:"primary_key"`
+	CustomerName string      `json:"customerName"`
+	OrderedAt    time.Time   `json:"orderedAt"`
+	Items        []Item      `json:"items" gorm:"foreignkey:OrderID"`
+}
